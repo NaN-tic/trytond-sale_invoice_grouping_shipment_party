a6cc9fa30d8be9f6556783a5c696370d09b11aab
Marina Nicolau <marina.nicolau@nan-tic.com>
2019-06-18 15:52:01 +0200

    Carrier for shipment party, allow in the configuration of the sale to define that the carrier loaded in the sale is the carrier of shipment party Task 037851

---
+++ configuration.py
@@ -0,0 +1,35 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+from trytond.model import fields, ModelSQL
+from trytond.pool import Pool, PoolMeta
+from trytond.modules.company.model import (
+    CompanyMultiValueMixin, CompanyValueMixin)
+
+__all__ = ['Configuration', 'ConfigurationCarrier']
+
+sale_default_party_carrier = fields.Selection([
+        (None, 'Party (default)'),
+        ('shipment_party', 'Shipment Party'),
+        ], 'Default Party Carrier')
+
+
+class Configuration(metaclass=PoolMeta):
+    __name__ = 'sale.configuration'
+    sale_default_party_carrier = fields.MultiValue(sale_default_party_carrier)
+
+    @classmethod
+    def multivalue_model(cls, field):
+        pool = Pool()
+        if field == 'sale_default_party_carrier':
+            return pool.get('sale.configuration.sale_carrier')
+        return super(Configuration, cls).multivalue_model(field)
+
+    @classmethod
+    def default_sale_party_carrier(cls, **pattern):
+        return cls.multivalue_model(
+            'sale_default_party_carrier').default_sale_party_carrier()
+
+
+class ConfigurationCarrier(metaclass=PoolMeta):
+    __name__ = 'sale.configuration.sale_carrier'
+    sale_default_party_carrier = sale_default_party_carrier
