5b7065b5406aaa227bf40607ceb97764681f18cf
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-28 12:52:35 +0200

    Replace test setuptools command by unittest discover

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,19 +1,15 @@
-# This file is part sale_invoice_grouping_shipment_party module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-import unittest
-import doctest
-import trytond.tests.test_tryton
+
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
 from trytond.tests.test_tryton import ModuleTestCase, with_transaction
-from trytond.tests.test_tryton import (doctest_setup, doctest_teardown,
-    doctest_checker)
 from trytond.pool import Pool
+from trytond.modules.company.tests import CompanyTestMixin
 
 
-class SaleInvoiceGroupingShipmentPartyTestCase(ModuleTestCase):
-    'Test Sale Invoice Grouping Shipment Party module'
+class SaleInvoiceGroupingShipmentPartyTestCase(CompanyTestMixin, ModuleTestCase):
+    'Test SaleInvoiceGroupingShipmentParty module'
     module = 'sale_invoice_grouping_shipment_party'
-
 
     @with_transaction()
     def test_sale(self):
@@ -49,13 +45,5 @@ class SaleInvoiceGroupingShipmentPartyTestCase(ModuleTestCase):
         sale.on_change_shipment_party()
         self.assertEqual(sale.party, party2)
 
-def suite():
-    suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            SaleInvoiceGroupingShipmentPartyTestCase))
-    suite.addTests(doctest.DocFileSuite(
-            'scenario_sale_invoice_grouping_shipment_party.rst',
-            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
-            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE,
-            checker=doctest_checker))
-    return suite
+
+del ModuleTestCase
